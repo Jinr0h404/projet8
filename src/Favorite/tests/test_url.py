8754eb8677b9b14a7cdf39fbd88044e70a6a44ed
ewen jeannenot <ewen.jeannenot@normandie.cci.fr>
2021-11-29 10:35:16 +0100

    change procfile due to error

--- src/Favorite/tests/test_url.py
+++
@@ -1,6 +0,0 @@
-from django.urls import resolve
-
-
-def test_index_url():
-    """Check if the name of the view is correct and that the URL matches the name of the view."""
-    assert resolve("/favoris/").view_name == "favorite-index"
--- src/Favorite/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import index
-"""the url file is used to associate an url path to a view with path"""
-
-urlpatterns = [
-    path("", index, name="favorite-index"),
-]
